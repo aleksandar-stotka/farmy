d9857994d608c2a122c374e332a1916b79821d50
aleksandar-stotka <aangelevski6@gmail.com>
2024-07-26 18:34:34 +0200

    use-getCollection--done

--- composables/getCollection.js
+++ composables/getCollection.js
@@ -1,36 +1,38 @@
-import { ref, watchEffect } from 'vue'
-import { projectFirestore } from '@/firebase/config'
+// getCollection.js
+import { ref, watchEffect } from 'vue';
+import { projectFirestore } from '@/firebase/config';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 
-const getCollection = (collection) => {
-
-  const documents = ref(null)
-  const error = ref(null)
+const getCollection = (collectionName) => {
+  const documents = ref(null);
+  const error = ref(null);
 
   // register the firestore collection reference
-  let collectionRef = projectFirestore.collection(collection)
-    .orderBy('createdAt')
+  const collectionRef = query(collection(projectFirestore, collectionName), orderBy('createdAt'));
 
-  const unsub = collectionRef.onSnapshot(snap => {
-    let results = []
-    snap.docs.forEach(doc => {
+  const unsub = onSnapshot(collectionRef, (snap) => {
+    let results = [];
+    snap.docs.forEach((doc) => {
       // must wait for the server to create the timestamp & send it back
-      doc.data().createdAt && results.push({...doc.data(), id: doc.id})
+      if (doc.data().createdAt) {
+        results.push({ ...doc.data(), id: doc.id });
+      }
     });
-    
+
     // update values
-    documents.value = results
-    error.value = null
-  }, err => {
-    console.log(err.message)
-    documents.value = null
-    error.value = 'could not fetch the data'
-  })
+    documents.value = results;
+    error.value = null;
+  }, (err) => {
+    console.log(err.message);
+    documents.value = null;
+    error.value = 'could not fetch the data';
+  });
 
   watchEffect((onInvalidate) => {
     onInvalidate(() => unsub());
   });
 
-  return { error, documents }
-}
+  return { error, documents };
+};
 
-export default getCollection
+export default getCollection;
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,10 +1,24 @@
+import { getCollection } from '#imports';
 <template>
-    <div>
-
-    </div>
+  <div v-if="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">
+        <h1>{{ doc.title }}</h1>
+      </div>
+      
+     </div>
 </template>
 
 <script setup>
+import { getCollection } from '#imports';
+
+const {error, documents} = getCollection("cawList")
+
+
+
+
+  
+
 
 
 
