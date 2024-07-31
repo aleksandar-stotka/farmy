6c2751dc148f41ed54a91698f478545947eaaf86
aleksandar-stotka <aangelevski6@gmail.com>
2024-07-31 23:35:38 +0200

    CREATE-BUTTON-HOME-PAGE

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
- index
+    <div class="container flex justify-center items-center min-h-screen">
+      <div class="flex flex-col space-y-4 md:flex-row md:space-x-8 md:space-y-0">
+        <nuxt-link to="/dashboard">
+          <button class="px-8 py-4 bg-blue-500 text-white text-xl rounded hover:bg-blue-600">DASHBOARD</button>
+        </nuxt-link>
+        <nuxt-link to="/create">
+          <button class="px-8 py-4 bg-green-500 text-white text-xl rounded hover:bg-green-600">CREATE</button>
+        </nuxt-link>
+      </div>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  
