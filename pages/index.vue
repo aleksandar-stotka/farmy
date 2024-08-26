414549a2ea0f23f47e2f8ae3db3ffd251c48effb
aleksandar-stotka <aangelevski6@gmail.com>
2024-08-26 09:32:33 +0200

    create-dashboard-bt

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
- 
+  <div class="flex items-center justify-center min-h-screen">
+    <nuxt-link to="/dashboard" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Go to Dashboard
+    </nuxt-link>
   </div>
 </template>
 
-<script setup>
-
+<script>
+export default {
+  // No additional logic required
+}
 </script>
 
 <style scoped>
-
+/* Additional custom styles can be added here if needed */
 </style>
