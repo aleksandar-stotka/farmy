b28f78e3caeedca9fbe5951df713c2bc6a0b19e3
aleksandar-stotka <aangelevski6@gmail.com>
2024-08-26 09:28:28 +0200

    fix-firebase-roles

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -3,6 +3,7 @@
    <span class="text-dark p-5" v-if="user">
         <span>Hello</span> {{ user.displayName }}!!
       </span>
+      <p v-if="error">{{ error.value }}</p>
     <div v-if="farmDocs">
       <FarmList :farmDocs="farmDocs" />
     </div>
@@ -11,10 +12,8 @@
   <script setup>
   import { getCollection } from '#imports';
   import { getUser } from '#imports';
-  import { ref, onMounted } from 'vue';
   
-  const displayNameFade = ref(true);
-  const show = ref(true);
+  
   
   
   
@@ -23,18 +22,6 @@
   </script>
   
   <style scoped>
-  .v-enter-active,
-  .v-leave-active {
-    transition: opacity 0.5s ease;
-  }
-  
-  .v-enter-from,
-  .v-leave-to {
-    opacity: 0;
-  }
-  
-  h2 {
-    transition: opacity 0.5s ease;
-  }
+ 
   </style>
   
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,13 @@
 <template>
-  <Transition>
-    <div 
-      class="h-screen flex justify-center items-center bg-cover bg-center" 
-      style="background-image: linear-gradient(to right, rgba(50, 149, 100, 0.8), rgba(90, 130, 180, 0.8)), url('/logo.png');" 
-      v-if="displayNameFade || show"
-    >
-      <h2 class="text-white text-8xl" v-if="user">
-        <span>Hello</span> {{ user.displayName }}!!
-      </h2>
-    </div>
-  </Transition>
-  
-  
+  <div>
+ 
+  </div>
 </template>
 
 <script setup>
-import { getCollection } from '#imports';
-import { getUser } from '#imports';
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
 
-
-const router = useRouter()
-
-const displayNameFade = ref(true);
-const show = ref(true);
-
-
-
-onMounted(() => {
-  setTimeout(() => {
-    displayNameFade.value = false;
-    show.value = false    }, 3000)
-
-    router.push('/dashboard');
-
-    
- 
-});
-
-const { user } = getUser();
-const { error, documents: farmDocs } = getCollection("cawList");
 </script>
 
 <style scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-
-h2 {
-  transition: opacity 0.5s ease;
-}
 </style>
